cc9d6507d382ad64722fb5654de2ae8ac77fe080
andrew <andrew@localhost>
2012-03-30 16:11:24 +0400

    gcc fix

--- unicode/detail/utf32.hpp
+++ unicode/detail/utf32.hpp
@@ -13,7 +13,7 @@ struct utf32
   template <typename InputIterator>
   static uint32_t read_code_point(InputIterator& in_iter)
   {
-    static_assert(sizeof(std::iterator_traits<InputIterator>::value_type) == sizeof(uint32_t), "UTF-32 error");
+    static_assert(sizeof(typename std::iterator_traits<InputIterator>::value_type) == sizeof(uint32_t), "UTF-32 error");
     const uint32_t cp(static_cast<uint32_t>(*in_iter)); ++in_iter;
     return cp;
   }
@@ -21,7 +21,7 @@ struct utf32
   template <typename OutputIterator>
   static void write_code_point(OutputIterator& out_iter, const uint32_t cp)
   {
-    static_assert(sizeof(std::iterator_traits<OutputIterator>::value_type) == sizeof(uint32_t), "UTF-32 error");
+    static_assert(sizeof(typename std::iterator_traits<OutputIterator>::value_type) == sizeof(uint32_t), "UTF-32 error");
     *out_iter = cp; ++out_iter;
   }
 }; // utf32
