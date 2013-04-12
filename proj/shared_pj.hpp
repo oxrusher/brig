33969ea3cb2008511745e4f8926c4d7e927f6e04
andrew <andrew@localhost>
2013-04-12 18:13:56 +0400

    PROJ.4 multithreading

--- proj/shared_pj.hpp
+++ proj/shared_pj.hpp
@@ -12,28 +12,35 @@
 namespace brig { namespace proj {
 
 class shared_pj {
-  std::shared_ptr<void> m_ptr;
+  std::shared_ptr<void> m_ctx, m_pj;
   void init(const char* def); // todo: MSVC November 2012 CTP - delegating constructors problem
 public:
   shared_pj()  {}
   explicit shared_pj(const char* def)  { init(def); }
   explicit shared_pj(const std::string& def)  { init(def.c_str()); }
   explicit shared_pj(int epsg)  { init(detail::def_from_epsg(epsg).c_str()); }
-  std::string get_def() const  { return m_ptr? detail::lib::singleton().p_pj_get_def(m_ptr.get(), 0): ""; }
-  bool is_latlong() const  { return m_ptr && detail::lib::singleton().p_pj_is_latlong(m_ptr.get()); }
-  operator projPJ() const  { return m_ptr.get(); }
-  bool operator ==(const shared_pj& r) const  { return m_ptr && r.m_ptr && (m_ptr.get() == r.m_ptr.get() || get_def().compare(r.get_def()) == 0); }
+  std::string get_def() const  { return m_pj? detail::lib::singleton().p_pj_get_def(m_pj.get(), 0): ""; }
+  bool is_latlong() const  { return m_pj && detail::lib::singleton().p_pj_is_latlong(m_pj.get()); }
+  operator projPJ() const  { return m_pj.get(); }
+  bool operator ==(const shared_pj& r) const  { return m_pj && r.m_pj && (m_pj.get() == r.m_pj.get() || get_def().compare(r.get_def()) == 0); }
   bool operator !=(const shared_pj& r) const  { return !operator ==(r); }
+  shared_pj copy() const  { return shared_pj(get_def()); }
 }; // shared_pj
 
 inline void shared_pj::init(const char* def)
 {
   using namespace std;
   using namespace detail;
-  if (!def || lib::singleton().empty()) throw runtime_error("projection error");
-  projPJ pj(lib::singleton().p_pj_init_plus( def ));
-  if (!pj) throw runtime_error("projection error");
-  m_ptr = shared_ptr<void>(pj, [](void* ptr){ lib::singleton().p_pj_free(ptr); });
+
+  if (!def || lib::singleton().empty()) throw runtime_error("proj error");
+
+  projCtx ctx(lib::singleton().p_pj_ctx_alloc());
+  if (!ctx) throw runtime_error("proj error");
+  m_ctx = shared_ptr<void>(ctx, [](void* ptr){ lib::singleton().p_pj_ctx_free(ptr); });
+
+  projPJ pj(lib::singleton().p_pj_init_plus_ctx(ctx, def));
+  if (!pj) throw runtime_error("proj error");
+  m_pj = shared_ptr<void>(pj, [](void* ptr){ lib::singleton().p_pj_free(ptr); });
 } // shared_pj::
 
 } } // brig::proj
--- proj/transform.hpp
+++ proj/transform.hpp
@@ -14,14 +14,14 @@ inline void transform(double* point_xy_begin, long point_xy_count, projPJ in_pj,
   using namespace detail;
 
   if (!in_pj || !out_pj)
-    throw runtime_error("projection error");
+    throw runtime_error("proj error");
 
   if (lib::singleton().p_pj_is_latlong(in_pj))
     for (long i(0), count(2*point_xy_count); i < count; ++i)
       point_xy_begin[i] *= DEG_TO_RAD;
 
   if (0 != lib::singleton().p_pj_transform(in_pj, out_pj, point_xy_count, 2, point_xy_begin, point_xy_begin + 1, 0))
-    throw runtime_error("projection error");
+    throw runtime_error("proj error");
 
   if (lib::singleton().p_pj_is_latlong(out_pj))
     for (long i(0), count(2*point_xy_count); i < count; ++i)
