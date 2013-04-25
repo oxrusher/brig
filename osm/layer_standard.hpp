7238671ac4125d2157c25d6ade69b16e529bcbc8
andrew <andrew@localhost>
2013-04-25 12:10:36 +0400

    cleaning

--- osm/layer_standard.hpp
+++ osm/layer_standard.hpp
@@ -1,15 +1,15 @@
 // Andrew Naplavkov
 
-#ifndef BRIG_OSM_LAYER_MAPNIK_HPP
-#define BRIG_OSM_LAYER_MAPNIK_HPP
+#ifndef BRIG_OSM_LAYER_STANDARD_HPP
+#define BRIG_OSM_LAYER_STANDARD_HPP
 
 #include <brig/osm/layer.hpp>
 #include <brig/string_cast.hpp>
 
 namespace brig { namespace osm {
 
-struct layer_mapnik : layer {
-  std::string get_name() override  { return "Mapnik"; }
+struct layer_standard : layer {
+  std::string get_name() override  { return "Standard"; }
   std::string get_url(size_t order, int x, int y, int z) override
   {
     std::string subdomain;
@@ -21,8 +21,8 @@ struct layer_mapnik : layer {
     }
     return "http://" + subdomain + ".tile.openstreetmap.org/" + string_cast<char>(z) + "/" + string_cast<char>(x) + "/" + string_cast<char>(y) + ".png";
   }
-}; // layer_mapnik
+}; // layer_standard
 
 } } // brig::osm
 
-#endif // BRIG_OSM_LAYER_MAPNIK_HPP
+#endif // BRIG_OSM_LAYER_STANDARD_HPP
