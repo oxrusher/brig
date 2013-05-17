54ddc55565e9459386a4caef769f98b530ca8481
andrew <andrew@localhost>
2013-05-17 14:26:37 +0400

    cleaning

--- proj/multithread_init.hpp
+++ proj/multithread_init.hpp
@@ -7,6 +7,10 @@
 
 namespace brig { namespace proj {
 
+// todo: MSVC November 2012 CTP - C++11 singleton problem
+// C++ 11 §6.7 [stmt.dcl] p4:
+// If control enters the declaration concurrently while the variable is being initialized, the concurrent execution shall wait for completion of the initialization.
+
 inline void multithread_init()
 {
   detail::lib::singleton();
